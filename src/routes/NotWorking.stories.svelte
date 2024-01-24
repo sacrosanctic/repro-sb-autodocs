077cbb3be784aa9bb2ccf7d6bb72d328388025a9
Scott Wu <sw@scottwu.ca>
2024-01-24 21:18:41 +0800

    add stories

---
+++ src/routes/NotWorking.stories.svelte
@@ -0,0 +1,21 @@
+<script context="module" lang="ts">
+  import type { Meta } from "@storybook/svelte";
+
+  import MyComponent from "./Counter.svelte";
+
+  export const meta: Meta = {
+    component: MyComponent,
+    tags: ["autodocs"],
+    argTypes: {},
+  };
+</script>
+
+<script lang="ts">
+  import { Story, Template } from "@storybook/addon-svelte-csf";
+</script>
+
+<Template let:args>
+  <MyComponent {...args} />
+</Template>
+
+<Story name="base" />
